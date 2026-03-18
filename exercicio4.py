e0883029ccae041e12c569d257341454def48783
daniel <161612167+D4NIELT0RRES@users.noreply.github.com>
2026-03-18 14:07:02 -0300

    Calculando a quantidade de plaquetas em diferentes unidades de medida

---
+++ exercicio4.py
@@ -0,0 +1,12 @@
+#====================================================================================================
+#  OBJETIVO: Calcular a quantidade de plaquetas por um dado fornecido pelo usuário
+#  DATA: 18/03/2025
+#  AUTOR: DANIEL GOMES TORRES
+#===================================================================================================== 
+
+plaqueta = 30000
+
+print("Plaquetas por litro", plaqueta)
+print("Plaquetas por decilitro", plaqueta/10)
+print("Plaquetas por mililitro", plaqueta/1000)
+
