b929ca93227c79dc4c2ed1acbe598c52433a9c5d
daniel <161612167+D4NIELT0RRES@users.noreply.github.com>
2026-03-18 13:28:24 -0300

    Calculando o imc do usuário

---
+++ exercicio1.py
@@ -0,0 +1,12 @@
+#*************************************************************************************************
+#  OBJETIVO: Cálculo do IMC do usuário
+#  DATA: 18/03/2025
+#  AUTOR: DANIEL GOMES TORRES
+#*================================================================================================ 
+
+peso = float(input("Digite o seu peso: "))
+altura = float(input("Digite a sua altura (ex: 1.75): "))
+
+imc = peso / (altura * altura)
+
+print(f"O seu IMC é {imc:.2f}")
