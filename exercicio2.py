2bc761b2888706583d1bca7cb06bd39ef3c13b1d
daniel <161612167+D4NIELT0RRES@users.noreply.github.com>
2026-03-18 13:39:02 -0300

    Calculando dívida e juros do usuário

---
+++ exercicio2.py
@@ -0,0 +1,20 @@
+#*************************************************************************************************
+#  OBJETIVO: Calculando quanto a pessoa vai estar devendo em 1, 2, 3 meses (DÍVIDA E JUROS)
+#  DATA: 18/03/2025
+#  AUTOR: DANIEL GOMES TORRES
+#*================================================================================================ 
+
+divida = float(input("Digite o valor da sua divida: ").replace(".", "").replace(",", "."))
+
+juros = 10
+
+aumento = divida * (juros/100)
+divida = divida + aumento
+
+aumento = divida * (juros/100)
+divida = divida + aumento
+
+aumento = divida * (juros/100)
+divida = divida + aumento
+
+print(f"O valor da sua dívida é de R${divida:.2f}")
