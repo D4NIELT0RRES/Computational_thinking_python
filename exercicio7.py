59535deef30506a05c7aa18639eb9f5bf12228da
daniel <161612167+D4NIELT0RRES@users.noreply.github.com>
2026-03-18 14:47:06 -0300

    Corrigindo o código do exercício 7

--- exercicio7.py
+++ exercicio7.py
@@ -4,14 +4,6 @@
 #  AUTOR: DANIEL GOMES TORRES
 #=====================================================================================================
 
-# nomePais = input("Digite o nome de um país: ")
-# pibAtual = float(input("Digite o PIB atual deste país: ").replace(".", "").replace(",", "."))
-
-# pibFormatado = f"{pibAtual:,.0f}".replace(",", ".")
-
-# print(f"No ano de 2024 o PIB do país {nomePais} era de {pibFormatado}")
-
-
 
 nomePais = input("Digite o nome do país: ")
 pib = float(input("Digite o PIB atual: ").replace(".", "").replace(",", "."))
