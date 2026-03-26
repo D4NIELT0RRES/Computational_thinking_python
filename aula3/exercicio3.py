e666ad8184b85fdf06a6e26e11816f51c1414095
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-25 22:34:44 -0300

    Desenvolvendo o exercicio 3 da aula 3, o objetivo é saber através da idade do usuário se o voto dele é opcional, obrigatório ou você tem o direito ao voto

---
+++ aula3/exercicio3.py
@@ -0,0 +1,32 @@
+#=========================================================================================
+#  OBJETIVO: 
+# 
+#           Dada uma idade de uma pessoa, crie os seguintes booleanos: 
+#           Pessoa pode entrar na cabine e votar (direito_voto)
+#           Pessoa tem a obrigação de votar (obrigacao_voto)
+#           Pessoa tem o voto, mas opcional (voto_opcional)
+#
+#  DATA: 25/03/2025
+#  AUTOR: DANIEL GOMES TORRES
+#=========================================================================================
+
+idade = int(input('Digite a sua idade: '))
+
+if (idade >= 16 and idade <18 ) or (idade <=70):
+    print('O seu voto é opcional!!')
+
+elif (idade >18) and (idade <70):
+    print('O seu voto é obrigatório!!')
+
+else:
+    print('Você ainda não pode votar')
+
+
+######################################################################
+
+
+idade = 16
+
+voto_opcional = idade >= 16 or idade < 18
+voto_obrigatorio = idade >= 18 or idade <= 70
+direito_voto = voto_opcional or voto_obrigatorio
