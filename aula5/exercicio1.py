ddcd1773e32add8728705222743f9e57184f6018
daniel <161612167+D4NIELT0RRES@users.noreply.github.com>
2026-04-08 14:39:23 -0300

    Base da linha de raciocinio do exercicio 1 da aula 5

---
+++ aula5/exercicio1.py
@@ -0,0 +1,13 @@
+#=====================================================================================================
+#  OBJETIVO: Recebe um nome e uma idade, retorna uma string falando se aquela pessoa pode ou não votar
+#  DATA: 08/04/2025
+#  AUTOR: DANIEL GOMES TORRES
+#=====================================================================================================
+
+
+def calculoIdade(nome, ano_nascimento):
+    idade = 2026 - ano_nascimento
+    print(f"{nome} você tem {idade} anos de idade")
+    return idade
+
+a = calculoIdade("Daniel", 2002)
