25012e29b0e65dcd26b6b55c1b00496ea23313c4
daniel <161612167+D4NIELT0RRES@users.noreply.github.com>
2026-04-08 14:41:51 -0300

    Melhorado a logica de raciocinio do exercicio 1 da aula 5

--- aula5/exercicio1.py
+++ aula5/exercicio1.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 #=====================================================================================================
 #  OBJETIVO: Recebe um nome e uma idade, retorna uma string falando se aquela pessoa pode ou não votar
 #  DATA: 08/04/2025
@@ -6,8 +7,10 @@
 
 
 def calculoIdade(nome, ano_nascimento):
-    idade = 2026 - ano_nascimento
+    agora = datetime.now()
+    anoAtual = agora.year
+    idade = anoAtual - ano_nascimento
     print(f"{nome} você tem {idade} anos de idade")
     return idade
 
-a = calculoIdade("Daniel", 2002)
+a = calculoIdade("Daniel", 2007)
